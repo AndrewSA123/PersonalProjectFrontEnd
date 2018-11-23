c900975dc3d8150a71ddbe1662084cba3a8f7f1a
andrewsa123 <andrewknott_123@hotmail.co.uk>
2018-11-23 13:56:40 +0000

    Fixing create

--- src/CreateMovie.js
+++ src/CreateMovie.js
@@ -68,9 +68,9 @@ class CreateMovie extends Component {
         var didvar = await axios.get(directorUrl).then((res) => {return res.data}); 
         var data = {
             title:titlevar,
-            aid:{aid:aidvar.aid,name:aidvar.name,age:aidvar.age},
-            gid:{gid:gidvar.gid,name:gidvar.name},
-            did:{did:gidvar.did,name:didvar.name,age:didvar.age}
+            aid:{aid:aidvar.ID,name:aidvar.Name,age:aidvar.Age},
+            gid:{gid:gidvar.ID,name:gidvar.Name},
+            did:{did:gidvar.ID,name:didvar.Name,age:didvar.Age}
         };
         console.log(data);
         axios.post(url, data).then((res) => {window.location.reload()});
